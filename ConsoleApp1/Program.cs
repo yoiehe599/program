466315e22a40538b7be4d3c8458e41ce00803e6e
pc <pc@PC-PC>
2019-09-16 16:22:57 +0800

    Hello from Wrox

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello from Wrox.");
+            Console.ReadLine();
+            return;
+        }
+    }
+}
