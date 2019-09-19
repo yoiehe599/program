20324190b73cd2c27c774f9e939bd00e851fd61b
yoiehe599 <yoie599@163.com>
2019-09-19 15:11:07 +0800

    int d

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int d;
+            d = 1;
+            Console.WriteLine(d);
+            Console.ReadLine();
+
+        }
+    }
+}
