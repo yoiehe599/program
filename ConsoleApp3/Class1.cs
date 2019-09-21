a912982942b15499a9be973f9389093a4c908a0b
yoiehe599 <yoie599@163.com>
2019-09-21 13:57:15 +0800

    int j+i;

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Class1
+    {
+        //public static int Main()
+        //{
+        //   for (int  i = 0; i < 10;i++)
+        //   {
+        //        int j = 30;
+        //        Console.WriteLine(j + i);
+        //    }
+        //    return 0;
+        //}
+    }
+}
---
+++ ConsoleApp3/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Class2
+    {
+        static int j = 20;
+        public static void Main()
+        {
+            int j = 30;
+            Console.WriteLine(j);
+            Console.WriteLine(Class2.j);
+        }
+    }
+}
